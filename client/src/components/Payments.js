032779980d9f9fefcedeab1468ee3915d6971174
Ben Hancock <hancocb@purdue.edu>
2019-09-10 13:55:24 -0400

    create payments component and import to header

---
+++ client/src/components/Payments.js
@@ -0,0 +1,14 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const Payments = () => {
+  return (
+    <StripeCheckout
+      amount={500}
+      token={token => console.log(token)}
+      stripeKey={process.env.REACT_APP_STRIPE_KEY}
+    />
+  );
+};
+
+export default Payments;
