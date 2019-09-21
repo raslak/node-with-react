8fd85f364edc9cbc3093ecf311822b769e0b0b25
Ben Hancock <hancocb@purdue.edu>
2019-09-21 17:08:28 -0400

    wrap up mailer, survey route, and survey template on backend

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
-import "materialize-css/dist/css/materialize.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import App from "./components/App";
-import reducers from "./reducers";
+import App from './components/App';
+import reducers from './reducers';
+
+// development only
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,5 +18,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
