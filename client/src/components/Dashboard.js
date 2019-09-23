89643bc52f6af9a8f9e08c095caab40e78de3c64
Ben Hancock <hancocb@purdue.edu>
2019-09-23 16:22:51 -0400

    update add button on dashboard to use router

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
       <div className="fixed-action-btn">
-        <a class="btn-floating btn-large waves-effect waves-light red">
-          <i class="material-icons">add</i>
-        </a>
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
       </div>
     </div>
   );
