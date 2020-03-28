28acc6c9f3be6f7f621d700bba8d18f117e7ff0b
Ben Hancock <hancocb@purdue.edu>
2020-03-27 20:15:20 -0400

    users can see survey results

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchSurveys } from '../../actions';
+
+const SurveyList = ({ fetchSurveys, surveys }) => {
+  useEffect(() => {
+    fetchSurveys();
+  }, [fetchSurveys]);
+
+  const renderSurveys = () => {
+    return surveys.map(survey => (
+      <div className="card darken-1" key={survey._id}>
+        <div className="card-content">
+          <span className="card-title">{survey.title}</span>
+          <p>{survey.body}</p>
+          <p className="right">
+            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="card-action">
+          <a>Yes: {survey.yes}</a>
+          <a>No: {survey.no}</a>
+        </div>
+      </div>
+    ));
+  };
+
+  return <div>{renderSurveys()}</div>;
+};
+
+const mapStateToProps = ({ surveys }) => {
+  return { surveys };
+};
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
